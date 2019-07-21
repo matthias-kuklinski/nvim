6ca77c949b6c7c9cf0bfbf35287457f3f74112ed
Matthias Kuklinski <contact@matthias-kuklinski.com>
2019-07-21 18:39:37 +0200

    feat: navigate splits

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+local keymap = vim.keymap
+
+-- Splits/Windows
+keymap.set("n", "<C-h>", "<C-w>h", { silent = true, desc = "Move to left window." })
+keymap.set("n", "<C-j>", "<C-w>j", { silent = true, desc = "Move to below window." })
+keymap.set("n", "<C-k>", "<C-w>k", { silent = true, desc = "Move to above window." })
+keymap.set("n", "<C-l>", "<C-w>l", { silent = true, desc = "Move to right window." })
