651cdcada19df03a8ae604b19bce52919e7801e6
Matthias Kuklinski <contact@matthias-kuklinski.com>
2019-07-23 20:05:57 +0200

    feat: clear highlights

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,6 @@ keymap.set("n", "<C-h>", "<C-w>h", { silent = true, desc = "Move to left window.
 keymap.set("n", "<C-j>", "<C-w>j", { silent = true, desc = "Move to below window." })
 keymap.set("n", "<C-k>", "<C-w>k", { silent = true, desc = "Move to above window." })
 keymap.set("n", "<C-l>", "<C-w>l", { silent = true, desc = "Move to right window." })
+
+-- Clear highlights
+keymap.set("n", "<Esc>", "<cmd>noh<CR>", { noremap = true, silent = true, desc = "Clear highlights." })
