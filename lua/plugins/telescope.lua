53cf03c676ec246d5d19922e62836b4eb55e3807
Matthias Kuklinski <contact@matthias-kuklinski.com>
2020-04-03 20:26:43 +0200

    feat: find files

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({
+        defaults = {
+          prompt_prefix = " ",
+          selection_caret = " ",
+          entry_prefix = " ",
+          layout_config = {
+            prompt_position = "top",
+          },
+          sorting_strategy = "ascending",
+        },
+      })
+
+      local keymap = vim.keymap
+      local api = require("telescope.builtin")
+      keymap.set("n", "<leader>ff", api.find_files, { silent = true, desc = "Find files." })
+    end,
+  },
+}
