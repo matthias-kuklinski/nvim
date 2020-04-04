ed6a07ac19f09d6b633a7c2f2c8112b3900d9f03
Matthias Kuklinski <contact@matthias-kuklinski.com>
2020-04-04 20:04:36 +0200

    feat: find current file in explorer sidebar

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -20,5 +20,17 @@ return {
       end,
       { silent = true, desc = "Toggle the nvim-tree sidebar." }
     )
+
+    keymap.set(
+      "n",
+      "<leader>ef",
+      function()
+        api.tree.open({
+          find_file = true,
+          focus = true,
+        })
+      end,
+      { silent = true, desc = "Open the nvim-tree sidebar and navigate to the file of the current buffer." }
+    )
   end,
 }
