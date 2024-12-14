6c8b3ca878fee98ceb18e2281df0515f1efe514c
Matthias Kuklinski <contact@matthias-kuklinski.com>
2024-12-14 14:34:32 +0100

    chore: format accordingly

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,20 +38,20 @@ return {
         },
       })
 
-      -- Setup nil language server (picked up automatically from PATH)
       lspconfig.nil_ls.setup({
         capabilities = capabilities,
         cmd = { "nil" },
         filetypes = { "nix" },
         -- TODO: review settings.
-        settings = {}
-          -- ["nil"] = {
-          --   formatting = {
-          --     -- Can be triggered via `:lua vim.lsp.buf.format()`
-          --     -- command = { "nixfmt" }
-          --   },
-          -- },
+        settings = {},
+        -- ["nil"] = {
+        --   formatting = {
+        --     -- Can be triggered via `:lua vim.lsp.buf.format()`
+        --     -- command = { "nixfmt" }
+        --   },
         -- },
+        -- },
+      })
       })
 
       lspconfig.yamlls.setup({
